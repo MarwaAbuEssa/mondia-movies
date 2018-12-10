ea13665d3615860e4e4d01f1e49ad59f394bdd25
MarwaAbuEssa <meromeroo@gmail.com>
2018-12-10 19:17:56 +0200

    modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
